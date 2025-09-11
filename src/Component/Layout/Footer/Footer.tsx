f90e5b9f1944bffa9716dd3007ba0c531d474073
WajeehaArain715 <arainwajeeha715@gmail.com>
2025-09-11 09:17:21 +0500

    my-commit

--- src/Component/Layout/Footer/Footer.tsx
+++ src/Component/Layout/Footer/Footer.tsx
@@ -6,12 +6,12 @@ import { FaFacebookF, FaInstagram, FaLinkedinIn, FaXTwitter } from "react-icons/
 
 export default function Footer() {
   return (
-    <footer className=" bg-[#eef6fb] text-[#051636] pt-18 pb-6 mt-20">
+    <footer className="bg-[#eef6fb] text-[#051636] pt-18 pb-6 mt-20">
       <div className="maincontainer">
         {/* Top Section */}
-        <div className=" grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-[81px]">
- 
-          <div>
+        <div className="flex flex-col lg:flex-row gap-10 lg:gap-[81px]">
+          {/* Logo + Text */}
+          <div className="flex-1">
             <div className="flex items-center">
               <Image
                 src="/image/Logo.png"
@@ -22,24 +22,22 @@ export default function Footer() {
                 priority
               />
             </div>
-            <p
-              className="mt-4 sm:mt-6 text-[15px] sm:text-[16px] lg:text-[18px] leading-[26px] text-[#051636] font-[400]
-                max-w-[720px]" >
+            <p className="mt-4 sm:mt-6 text-[15px] sm:text-[16px] lg:text-[18px] leading-[26px] 
+               text-[#051636] font-[400] lg:max-w-[720px]">
               Lorem ipsum dolor sit amet consectetur. Id leo amet cursus non ut.
               Pretium nisl mauris nunc etiam sit quam nibh duis sollicitudin. Pretium nisl mauris
               Lacus suspendisse venenatis sed aliquam magnis at. Aenean vel ut nunc aenean tincidunt imperdiet.
             </p>
-
           </div>
 
-          {/*  Links */}
-          <div className="grid grid-cols-2 gap-6 sm:gap-[15px] text-[#051636] text-[15px] sm:text-[16px] font-[400] leading-[100%] mt-6 lg:mt-0">
-            <div className="space-y-3 sm:space-y-[35px] flex flex-col">
+          {/* Links */}
+          <div className="flex flex-1 justify-around text-[#051636] text-[15px] sm:text-[16px] font-[400] mt-6 lg:mt-0">
+            <div className="flex flex-col space-y-3 sm:space-y-[35px]">
               <a href="#" className="hover:text-orange-500 transition">Home</a>
               <a href="#" className="hover:text-orange-500 transition">Services</a>
               <a href="#" className="hover:text-orange-500 transition">Industries</a>
             </div>
-            <div className="space-y-3 sm:space-y-[35px] flex flex-col">
+            <div className="flex flex-col space-y-3 sm:space-y-[35px]">
               <a href="#" className="hover:text-orange-500 transition">Why KMC</a>
               <a href="#" className="hover:text-orange-500 transition">Offices</a>
               <a href="#" className="hover:text-orange-500 transition">About</a>
@@ -47,7 +45,7 @@ export default function Footer() {
           </div>
         </div>
 
-       
+        {/* Divider */}
         <div className="border-t border-[#757575] my-6 sm:my-8 w-full"></div>
 
         {/* Bottom */}
